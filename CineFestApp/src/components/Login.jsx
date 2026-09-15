ba094f021664853391316b9db107725b502c0855
144bharat <144bharatgautam@gmail.com>
2026-09-15 11:10:22 +0530

    1. Add Validation for signup/signin form with useRef hook. 2. provided firebase backend to the app. 3. deployed app to production.

--- CineFestApp/src/components/Login.jsx
+++ CineFestApp/src/components/Login.jsx
@@ -1,13 +1,51 @@
 import Header from "./Header";
 import loginBanner from "../assets/login-banner.jpg";
-import { useState } from "react";
+import { useRef, useState } from "react";
+import { validateSignUpFormData, validateSignInFormData } from "../utils/validateUserData";
 
 const Login = () => {
 
   const [isSignInForm, setIsSignInForm] = useState(true);
+  const [errorObj, setErrorObj] = useState({
+    isAnyValidationError: false,
+    name: null,
+    email: null,
+    password: null
+  });
+
+  const nameRef = useRef(null);
+  const emailRef = useRef(null);
+  const passwordRef = useRef(null);
+
 
   function toggleSignInForm() {
+    setErrorObj({
+    isAnyValidationError: false,
+    name: null,
+    email: null,
+    password: null
+  });
     setIsSignInForm(!isSignInForm);
+  }
+
+  const handleSignInSignUpSubmit = (e) => {
+    // HERE I AM RESTRCTING THE DEFAULT FORM SUBMIT BECAUSE I NEED TO VALIDATE FORM FIRST.
+    e.preventDefault();
+    
+    // VALIDATE FORM DATA
+    //console.log(nameRef.current.value.trim());
+    
+    let validationErrorObj = {};
+
+    if(isSignInForm){
+      validationErrorObj = validateSignInFormData(emailRef.current.value.trim(), passwordRef.current.value.trim(), isSignInForm)
+    }else{
+      validationErrorObj = validateSignUpFormData(nameRef?.current?.value?.trim(), emailRef.current.value.trim(), passwordRef.current.value.trim(), isSignInForm)
+      console.log(validationErrorObj);
+
+    }
+
+    setErrorObj(validationErrorObj);
   }
 
 return (
@@ -18,32 +56,45 @@ return (
       <Header />
       
       <section className="relative flex min-h-screen items-center justify-center">
-        <form className="w-full max-w-md space-y-6 rounded-lg bg-black/80 p-8 sm:p-10">
+        <form onSubmit={(e) => handleSignInSignUpSubmit(e)} className="w-full max-w-md space-y-6 rounded-lg bg-black/80 p-8 sm:p-10">
           <h2 className="text-3xl font-bold text-white"> { isSignInForm? 'Sign In': 'Sign Up' } </h2>
 
           <div className="space-y-4">
             { !isSignInForm? 
+            
             <input
+              ref={nameRef}
               type="text"
               placeholder="Full Name"
               className="w-full rounded bg-neutral-700 px-4 py-3 text-white placeholder-neutral-400 outline-none focus:ring-2 focus:ring-primary font-serif"
-            />
+            />           
             : null
             }
+
+            {(!isSignInForm && errorObj.name !== null)? <p className="text-primary font-extrabold text-sm">{errorObj.name}</p>: null}
+
             <input
+              ref={emailRef}
               type="email"
               placeholder="Email"
               className="w-full rounded bg-neutral-700 px-4 py-3 text-white placeholder-neutral-400 outline-none focus:ring-2 focus:ring-primary font-serif"
             />
+            
+            {errorObj.email !== null? <p className="text-primary font-extrabold text-sm">{errorObj.email}</p>: null}
+
             <input
+              ref={passwordRef}
               type="password"
               placeholder="Password"
               className="w-full rounded bg-neutral-700 px-4 py-3 text-white placeholder-neutral-400 outline-none focus:ring-2 focus:ring-primary font-serif"
             />
+            
+            {errorObj.password !== null? <p className="text-primary font-extrabold text-sm">{errorObj.password}</p>: null}
+
           </div>
           <button
             type="submit"
-            className="w-full rounded bg-primary py-3 font-semibold text-white transition hover:opacity-90"
+            className="w-full rounded bg-primary py-3 font-semibold text-white transition hover:opacity-90 cursor-pointer"
           >
              { isSignInForm? 'Sign In': 'Sign Up' } 
           </button>
---
+++ CineFestApp/src/utils/validateUserData.js
@@ -0,0 +1,108 @@
+export const validateSignUpFormData = (name, email, password) => {
+
+let errorObj = {
+    isAnyValidationError: false,
+    name: null,
+    email: null,
+    password: null
+}
+
+const nameErrors = [];
+
+if(name.length < 3 || name.length >15){
+    nameErrors.push("• Must be 3–16 characters");
+}
+
+if(!/^[a-zA-Z0-9_-]+$/.test(name)){
+    nameErrors.push("• Can contain only letters, numbers, _ and -");
+}
+
+if(nameErrors.length > 0){
+    errorObj.isAnyValidationError = true;
+
+    if(name.length === 0){
+    errorObj.name = "Full Name is required.";    
+    }else{
+        errorObj.name = `Full Name is Invalid:\n${nameErrors.join("\n")}`; 
+    }
+}
+
+
+const isEmailValid = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email);
+let emailErrors = [];
+
+if(!isEmailValid){
+    emailErrors = ["• Must contain a valid email format", "• Must have @", "• Must have a valid domain such as .com"];
+    
+    if(email.length === 0){
+    errorObj.email = "Email Address is required.";    
+    }else{
+    errorObj.email =  `Email Address Is Invalid:\n${emailErrors.join("\n")}`;
+    }
+    errorObj.isAnyValidationError = true;
+}
+
+const passwordErrors = [];
+
+if (password.length < 8) {
+    passwordErrors.push("• Must be at least 8 characters");
+}
+
+if (!/[A-Z]/.test(password)) {
+    passwordErrors.push("• Must contain at least one uppercase letter");
+}
+
+if (!/[a-z]/.test(password)) {
+    passwordErrors.push("• Must contain at least one lowercase letter");
+}
+
+if (!/[0-9]/.test(password)) {
+    passwordErrors.push("• Must contain at least one number");
+}
+
+if (!/[!@#$%^&*(),.?":{}|<>_\-]/.test(password)) {
+    passwordErrors.push("• Must contain at least one special character");
+}
+
+if (passwordErrors.length > 0) { 
+    errorObj.isAnyValidationError = true;
+    
+    if(password.length === 0){
+    errorObj.password = "Password is required.";    
+    }else{
+    errorObj.password =  `Password Is Invalid:\n${passwordErrors.join("\n")}`;
+    }
+}
+
+
+return errorObj;
+}
+
+
+export const validateSignInFormData = (email, password) => {
+
+let errorObj = {
+    isAnyValidationError: false,
+    email: null,
+    password: null
+}
+
+if(email.length === 0){
+    errorObj.isAnyValidationError = true;
+    errorObj.email = "Email Address is required.";
+}else if(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)){
+    errorObj.isAnyValidationError = true;
+    errorObj.email = "Email Address is Invalid.";
+}
+
+if(password.length === 0){
+    errorObj.isAnyValidationError = true;
+    errorObj.password = "Password Address is required.";
+}
+// else if(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/.test(email)){
+//     errorObj.email = "Email Address is Invalid.";
+// }
+
+return errorObj;
+
+}
